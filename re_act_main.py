28e557cdc6a190854eed622932de303726cd3a57
mdshadanaltmash <mdshadanaltmash44@gmail.com>
2026-04-03 12:09:08 +0530

    integrating Tivoli

--- re_act_main.py
+++ re_act_main.py
@@ -5,18 +5,23 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import OpenAI
 import os
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+
+
 load_dotenv()
+# tavily = TavilyClient()
 
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over the internet
-    :param query: The query to search for
-    :return: The search result
-    """
-    print(f"Searching for {query}")
-    return "Tokyo weather is sunny"
+# @tool
+# def search(query: str) -> dict:
+#     """
+#     Tool that searches over the internet
+#     :param query: The query to search for
+#     :return: The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query)
 
 
 llm = AzureChatOpenAI(
@@ -24,16 +29,16 @@ llm = AzureChatOpenAI(
     api_version=os.environ["AZURE_OPENAI_API_VERSION"],
 )
 print(llm.invoke("Hello Shadan"))
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain course")
-    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokoyo?")]})
+    msg = {"messages": [HumanMessage(content="What is the weather in Delhi, India?")]}
+    result = agent.invoke(input=msg)
     print(result)
 
 
 if __name__ == "__main__":
     main()
-    print('hi')
